ea80fe7643faa9bec37dfa4e16a4725d887e4cd7
Caitlin Barron <barroncn@gmail.com>
2017-12-13 23:32:35 +0000

    Connecting Heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    //port: process.env.PORT || 8081, //Do I need this? in mvc example they used two diff ports for server.js and connection.js??
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "pasta_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else{
+    connection = mysql.createConnection({
+                                          host: "localhost",
+                                          user: "root",
+                                          password: "",
+                                          database: "pasta_db"
+                                        });
+}
+ 
 
 connection.connect(function(err){
     if (err) return err;
